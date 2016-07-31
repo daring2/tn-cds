917242b0303d73b03c328b69f71a0506892a6b59
daring <danila.daring@gmail.com>
2016-07-31 21:32:04 +0300

    issue #1 - refactoring

--- src/main/java/ru/antinform/cds/test/TestApp.java
+++ src/main/java/ru/antinform/cds/test/TestApp.java
@@ -1,10 +1,17 @@
 package ru.antinform.cds.test;
 
+import org.slf4j.Logger;
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class TestApp {
 
-	public static void main(String[] args) throws Exception {
+	private static final Logger log = getLogger(TestApp.class);
+
+	public static void main(String[] args) {
 		try (TestContext ctx = new TestContext()) {
 			new SaveTest(ctx).run();
+		} catch (Exception e) {
+			log.error("run error", e);
 		}
 	}
 
