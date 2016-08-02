8ac26d1e5c5a19419104afa19a94f122a026eb3e
daring <danila.daring@gmail.com>
2016-08-02 16:18:35 +0300

    issue #3 - refactoring

--- src/main/java/ru/antinform/cds/test/TagDataServiceTest.java
+++ src/main/java/ru/antinform/cds/test/TagDataServiceTest.java
@@ -1,10 +1,17 @@
 package ru.antinform.cds.test;
 
 import com.datastax.driver.core.Session;
+import org.slf4j.Logger;
+import ru.antinform.cds.domain.TagData;
 import ru.antinform.cds.domain.TagDataService;
+import java.util.stream.Stream;
 import static java.util.concurrent.TimeUnit.MINUTES;
+import static java.util.stream.Collectors.joining;
+import static org.slf4j.LoggerFactory.getLogger;
 
 class TagDataServiceTest {
+
+	private final Logger log = getLogger(getClass());
 
 	private final Context ctx;
 
@@ -17,7 +24,8 @@ class TagDataServiceTest {
 		long endTime = time + MINUTES.toMillis(1);
 		long r1 = ctx.tagDataService().findByPeriod(time + 1, endTime).count();
 		if (r1 != 0) throw new RuntimeException("r1 = " + r1);
-		ctx.tagDataService().findByPeriod(time, endTime).limit(5).forEach(System.out::println);
+		Stream<TagData> rs = ctx.tagDataService().findByPeriod(time, endTime).limit(5);
+		log.info(rs.map(Object::toString).collect(joining(", ")));
 	}
 
 	interface Context {
