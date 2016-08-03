863b8a77068e253e535236e8ffdd980f60e610e4
daring <danila.daring@gmail.com>
2016-08-03 15:56:46 +0300

    issue #3 - refactoring

--- src/main/java/ru/antinform/cds/test/TestApp.java
+++ src/main/java/ru/antinform/cds/test/TestApp.java
@@ -1,6 +1,9 @@
 package ru.antinform.cds.test;
 
 import org.slf4j.Logger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Future;
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class TestApp {
@@ -9,10 +12,12 @@ public class TestApp {
 
 	public static void main(String[] args) {
 		try (TestContext ctx = new TestContext()) {
-//			new QueryTagDataTest(ctx).start();
+			List<Future<?>> results = new ArrayList<>();
+			results.add(new QueryTagDataTest(ctx).start());
 			new SaveTagDataTest(ctx).run();
-			new QueryTagDataTest(ctx).run();
-			new TagDataServiceTest(ctx).run();
+//			new QueryTagDataTest(ctx).run();
+//			new TagDataServiceTest(ctx).run();
+			for (Future<?> r : results) r.get();
 		} catch (Exception e) {
 			log.error("run error", e);
 		}
