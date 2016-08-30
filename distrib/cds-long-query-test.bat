2e3586541c5f8e42ce48688fbeb08296d9e9ed58
daring <danila.daring@gmail.com>
2016-08-30 16:23:49 +0300

    issue #7 - refactoring

--- distrib/cds-long-query-test.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-cd /d %~dp0
-call cds-test.bat -Dcds.test.enabledTests="LongQueryTest,ParallelQueryTest" %*
--- distrib/cds-save-test-1m.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-cd /d %~dp0
-set TestOpts=-Dcds.test.SaveTagDataTest.runTime=1m -Dcds.test.QueryTest.runTime=1m
-call cds-save-test.bat %TestOpts% %*
