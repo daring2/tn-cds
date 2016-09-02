701c315fd7e4e7eda76a5bd93a8f2e13b45baee7
daring <danila.daring@gmail.com>
2016-09-02 17:27:07 +0300

    issue #9 - refactoring

--- src/main/java/ru/antinform/cds/formula/MovingAverage.java
+++ src/main/java/ru/antinform/cds/formula/MovingAverage.java
@@ -2,14 +2,12 @@ package ru.antinform.cds.formula;
 
 import org.apache.commons.math3.stat.StatUtils;
 
-@SuppressWarnings("WeakerAccess")
 public class MovingAverage extends WindowFunction {
 
 	public MovingAverage(int size) {
 		super(size);
 	}
 
-	@Override
 	double update() {
 		return StatUtils.mean(values, 0, dataSize);
 	}
--- src/main/java/ru/antinform/cds/formula/StdDeviation.java
+++ src/main/java/ru/antinform/cds/formula/StdDeviation.java
@@ -1,26 +1,16 @@
 package ru.antinform.cds.formula;
 
 import org.apache.commons.math3.stat.StatUtils;
-import static java.lang.Integer.min;
 import static java.lang.Math.sqrt;
 
-@SuppressWarnings("WeakerAccess")
-public class StdDeviation implements Formula {
-
-	final int size;
-	final double[] values;
-
-	int index;
+public class StdDeviation extends WindowFunction {
 
 	public StdDeviation(int size) {
-		this.size = size;
-		values = new double[size];
+		super(size);
 	}
 
-	@Override
-	public double apply(double value) {
-		values[index++ % size] = value;
-		return sqrt(StatUtils.variance(values, 0, min(index, size)));
+	double update() {
+		return sqrt(StatUtils.variance(values, 0, dataSize));
 	}
 
 }
