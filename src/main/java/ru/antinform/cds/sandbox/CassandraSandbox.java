92a6911e33fd029c579f6903fee6fa0c4a7d739a
daring <danila.daring@gmail.com>
2016-09-06 10:37:07 +0300

    issue #10 - refactoring

--- src/main/java/ru/antinform/cds/sandbox/CassandraSandbox.java
+++ src/main/java/ru/antinform/cds/sandbox/CassandraSandbox.java
@@ -1,17 +1,12 @@
 package ru.antinform.cds.sandbox;
 
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.Session;
+import ru.antinform.cds.MainContext;
 
 public class CassandraSandbox {
 
 	public static void main(String[] args) throws Exception {
-		String address = "127.0.0.1";
-//		String address = "192.168.100.144";
-		Cluster.Builder clusterBuilder = Cluster.builder().addContactPoint(address);
-		try (Cluster cluster = clusterBuilder.build()) {
-			Session session = cluster.connect("cds");
-			new CassandraInsertTest(session, 1000, 1000).run();
+		try (MainContext ctx = new MainContext()) {
+			new CassandraInsertTest(ctx.session(), 1000, 1000).run();
 		}
 	}
 
